12cd9531a92a4744663b199a183ea10c9034efbc
Cameron Lynch <clynch@jahnelgroup.com>
2023-06-15 11:47:10 -0400

    Initial Setup

---
+++ src/main/java/com/modules/demo/ApiModuleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.modules.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModuleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiModuleDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/modules/demo/Pet.java
@@ -0,0 +1,3 @@
+package com.modules.demo;
+
+public record Pet(long id, String name, String breed, int age) { }
